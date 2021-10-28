31433a2fe0d818d70b9d7a4f54e284f5fb57228a
SnowballSH <66022611+SnowballSH@users.noreply.github.com>
2021-10-28 14:33:21 -0500

    lances

---
+++ pages/tutorial/pieces/lance.tsx
@@ -0,0 +1,89 @@
+import { Text, Container } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../../../styles/Pieces.module.scss";
+import { ShogiTheme } from "../../../theme";
+import { Config } from "chessgroundx/config";
+import ShogiWithLogic from "../../../components/shogilogic";
+
+const Lance: NextPage = () => {
+  const FEN = "9/9/9/9/4p4/8S/9/9/L3L3L b - 1";
+
+  const CFG: Config = {
+    drawable: {
+      autoShapes: [
+        {
+          orig: "a1",
+          dest: "a9",
+          brush: "green",
+        },
+        {
+          orig: "e1",
+          dest: "e5",
+          brush: "green",
+        },
+        {
+          orig: "i1",
+          dest: "i3",
+          brush: "green",
+        },
+        {
+          orig: "i3",
+          dest: "i4",
+          brush: "red",
+        },
+      ],
+    },
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Shogi Learn - Lances</title>
+        <meta name="description" content="Shogi Lances" />
+      </Head>
+
+      <main className={styles.main}>
+        <Container maxW="container.lg" centerContent>
+          <Text
+            fontSize="7xl"
+            color={ShogiTheme.colors["sPink"][400]}
+            className={styles.pieceTitle}
+          >
+            Lance (香)
+          </Text>
+
+          <Container
+            p="4rem"
+            maxW="100%"
+            boxShadow="xl"
+            backgroundColor="#f3ffe0"
+            rounded="lg"
+          >
+            <Text
+              fontSize="5xl"
+              fontWeight="bold"
+              color={ShogiTheme.colors["sGreen"][400]}
+            >
+              Basic Movement
+            </Text>
+
+            <Container maxW="container.md" pt={4} pb={10} centerContent>
+              <Text fontSize="3xl">
+                Lances move any amount of squares forward (toward enemy).
+              </Text>
+              <Text fontSize="3xl">
+                Lances cannot capture ally pieces and cannot jump over any piece.
+              </Text>
+            </Container>
+            <Container maxW="fit-content" centerContent>
+              <ShogiWithLogic editable fen={FEN} config={CFG} />
+            </Container>
+          </Container>
+        </Container>
+      </main>
+    </div>
+  );
+};
+
+export default Lance;
